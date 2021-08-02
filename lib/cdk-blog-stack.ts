924b0cf74a8c40ea1344f3cd02cec43bdc3662eb
Brandon Ly <wowbob396@gmail.com>
2021-08-02 17:24:29 -0500

    added example lambda

--- lib/cdk-blog-stack.ts
+++ lib/cdk-blog-stack.ts
@@ -1,9 +1,23 @@
 import * as cdk from '@aws-cdk/core';
+import { Bucket } from '@aws-cdk/aws-s3';
+import { Code, Function, Runtime } from '@aws-cdk/aws-lambda';
 
 export class CdkBlogStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
     // The code that defines your stack goes here
+    const bucket = new Bucket(this, 'test-bucket');
+
+    const writeToBucketFunction = new Function(this, 'write-function', {
+      code: Code.fromAsset('./dist/src/'),
+      handler: 'write-bucket.handler',
+      runtime: Runtime.NODEJS_14_X,
+      environment: {
+        BUCKET_NAME: bucket.bucketName,
+      }
+    });
+
+    bucket.grantWrite(writeToBucketFunction);
   }
 }
---
+++ src/write-bucket.ts
@@ -0,0 +1,25 @@
+import { S3 } from 'aws-sdk';
+import { APIGatewayProxyEvent, APIGatewayProxyResult } from 'aws-lambda';
+
+const BUCKET_NAME = process.env.BUCKET_NAME!;
+
+export const handler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
+
+  const s3Client = new S3();
+
+  console.log(event);
+  
+  try {
+    await s3Client.putObject({
+      Bucket: BUCKET_NAME,
+      Key: "test.json",
+      Body: JSON.stringify({
+        hello: "world",
+      }),
+    }).promise();
+    return Promise.resolve({statusCode: 200, body: "File Created!"});
+  } catch (error) {
+    console.error('Could not list items from S3 bucket', error);
+    throw error;
+  }
+}
