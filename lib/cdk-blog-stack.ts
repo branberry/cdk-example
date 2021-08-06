e5f602e41eeae5926e92b172986eb8527ba367ae
Brandon Ly <wowbob396@gmail.com>
2021-08-05 19:28:53 -0500

    added api endpoit

--- lib/cdk-blog-stack.ts
+++ lib/cdk-blog-stack.ts
@@ -1,6 +1,7 @@
 import * as cdk from '@aws-cdk/core';
 import { Bucket } from '@aws-cdk/aws-s3';
 import { Code, Function, Runtime } from '@aws-cdk/aws-lambda';
+import { LambdaIntegration, RestApi } from '@aws-cdk/aws-apigateway';
 
 export class CdkBlogStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
@@ -8,6 +9,8 @@ export class CdkBlogStack extends cdk.Stack {
 
     // The code that defines your stack goes here
     const bucket = new Bucket(this, 'test-bucket');
+
+    const restApi = new RestApi(this, 'test-api');
 
     const writeToBucketFunction = new Function(this, 'write-function', {
       code: Code.fromAsset('./dist/src/'),
@@ -19,5 +22,7 @@ export class CdkBlogStack extends cdk.Stack {
     });
 
     bucket.grantWrite(writeToBucketFunction);
+
+    restApi.root.addMethod("POST", new LambdaIntegration(writeToBucketFunction));
   }
 }
